3ce90e62fee18c06c5c4faea9349c6b8d7a44ff3
Viacheslav Otamas <newstageja@gmail.com>
2020-05-29 13:09:04 +0300

    Migrate from MySQL to PostgeSQL

---
+++ src/main/resources/db/migration/V280520202230__initialize_schema.sql
@@ -0,0 +1,47 @@
+CREATE TABLE category
+(
+    id            serial                 NOT NULL,
+    category_name character varying(255) NOT NULL,
+    CONSTRAINT category_pkey PRIMARY KEY (id)
+);
+
+CREATE TABLE project
+(
+    id          serial  NOT NULL,
+    created_at  timestamp,
+    name        character varying(255) DEFAULT NULL,
+    price       real,
+    quantity    integer,
+    updated_at  timestamp,
+    category_id integer NOT NULL,
+    CONSTRAINT project_pkey PRIMARY KEY (id),
+    CONSTRAINT project_category_id_fkey FOREIGN KEY (category_id)
+        REFERENCES category (id)
+);
+
+CREATE TABLE "user"
+(
+    id       bigserial NOT NULL,
+    email    character varying(255) DEFAULT NULL,
+    username character varying(255) DEFAULT NULL,
+    password character varying(255) DEFAULT NULL,
+    CONSTRAINT user_pkey PRIMARY KEY (id),
+    CONSTRAINT user_email_key UNIQUE (email)
+);
+
+CREATE TABLE "role"
+(
+    id   bigserial NOT NULL,
+    name character varying(255) DEFAULT NULL,
+    CONSTRAINT role_pkey PRIMARY KEY (id)
+);
+
+CREATE TABLE users_roles
+(
+    user_id bigint NOT NULL,
+    role_id bigint NOT NULL,
+    CONSTRAINT role_id_fkey FOREIGN KEY (role_id)
+        REFERENCES "role" (id),
+    CONSTRAINT user_id_fkey FOREIGN KEY (user_id)
+        REFERENCES "user" (id)
+);
