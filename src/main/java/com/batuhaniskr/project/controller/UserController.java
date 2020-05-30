acebd9948f2c69d9aa96cbc67245ab905f414cd6
Viacheslav Otamas <newstageja@gmail.com>
2020-05-30 23:45:03 +0300

    Refactored UserController, UserService, UserServiceImpl

--- src/main/java/com/batuhaniskr/project/controller/UserController.java
+++ src/main/java/com/batuhaniskr/project/controller/UserController.java
@@ -3,7 +3,6 @@ package com.batuhaniskr.project.controller;
 import com.batuhaniskr.project.dto.UserRegistrationDto;
 import com.batuhaniskr.project.model.User;
 import com.batuhaniskr.project.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -16,7 +15,6 @@ public class UserController {
 
     private UserService userService;
 
-    @Autowired
     public UserController(UserService userService) {
         this.userService = userService;
     }
