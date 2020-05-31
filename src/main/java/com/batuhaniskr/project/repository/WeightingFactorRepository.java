0ee92272e21c8ae3efd51d6177715c8ce3960eea
Viacheslav Otamas <newstageja@gmail.com>
2020-05-31 13:20:29 +0300

    Added CommonPersonalDataRepository, CommonProfessionalDataRepository, WeightingFactorRepository

---
+++ src/main/java/com/batuhaniskr/project/repository/WeightingFactorRepository.java
@@ -0,0 +1,7 @@
+package com.batuhaniskr.project.repository;
+
+import com.batuhaniskr.project.model.WeightingFactor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WeightingFactorRepository extends JpaRepository<WeightingFactor, Long> {
+}
