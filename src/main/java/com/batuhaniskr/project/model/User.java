43a0df883fff39b393aa620dbffc62cdf8ddec77
Viacheslav Otamas <newstageja@gmail.com>
2020-06-01 18:07:06 +0300

    Changed id generation type

--- src/main/java/com/batuhaniskr/project/model/User.java
+++ src/main/java/com/batuhaniskr/project/model/User.java
@@ -15,7 +15,7 @@ import java.util.Collection;
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(name = "username")
