4adc58186b39ef9102992e4eabc9bf11bc686e8e
Viacheslav Otamas <newstageja@gmail.com>
2020-06-06 00:01:33 +0300

    Added project_employee_role_id column to employee table

--- src/main/resources/db/migration/V290520201914__add_tables.sql
+++ src/main/resources/db/migration/V290520201914__add_tables.sql
@@ -22,65 +22,6 @@ CREATE TABLE weighting_factor
     PRIMARY KEY (id)
 );
 
-CREATE TABLE employee
-(
-    id         bigserial              NOT NULL,
-    email      character varying(255) NOT NULL,
-    first_name character varying(255) NOT NULL,
-    last_name  character varying(255) NOT NULL,
-    position   character varying(255) NOT NULL,
-    project_id bigint DEFAULT NULL,
-    UNIQUE (email),
-    PRIMARY KEY (id),
-    FOREIGN KEY (project_id)
-        REFERENCES project (id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE SET DEFAULT
-        NOT VALID
-);
-
-CREATE TABLE employee_personal_data
-(
-    id                      bigserial NOT NULL,
-    common_personal_data_id bigint    NOT NULL,
-    weighting_factor_id     bigint    NOT NULL,
-    employee_id             bigint    NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (employee_id)
-        REFERENCES employee (id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE CASCADE,
-    FOREIGN KEY (common_personal_data_id)
-        REFERENCES common_personal_data (id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE CASCADE,
-    FOREIGN KEY (weighting_factor_id)
-        REFERENCES weighting_factor (id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE CASCADE
-);
-
-CREATE TABLE employee_professional_data
-(
-    id                          bigserial NOT NULL,
-    common_professional_data_id bigint    NOT NULL,
-    weighting_factor_id         bigint    NOT NULL,
-    employee_id                 bigint    NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (employee_id)
-        REFERENCES employee (id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE CASCADE,
-    FOREIGN KEY (common_professional_data_id)
-        REFERENCES common_professional_data (id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE CASCADE,
-    FOREIGN KEY (weighting_factor_id)
-        REFERENCES weighting_factor (id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE CASCADE
-);
-
 CREATE TABLE project_employee_role
 (
     id               bigserial              NOT NULL,
@@ -132,6 +73,71 @@ CREATE TABLE project_employee_role_professional_data
         ON DELETE CASCADE,
     FOREIGN KEY (project_employee_role_id)
         REFERENCES project_employee_role (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE CASCADE
+);
+
+CREATE TABLE employee
+(
+    id                       bigserial              NOT NULL,
+    email                    character varying(255) NOT NULL,
+    first_name               character varying(255) NOT NULL,
+    last_name                character varying(255) NOT NULL,
+    position                 character varying(255) NOT NULL,
+    project_employee_role_id bigint DEFAULT NULL,
+    project_id               bigint DEFAULT NULL,
+    UNIQUE (email),
+    PRIMARY KEY (id),
+    FOREIGN KEY (project_id)
+        REFERENCES project (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE SET DEFAULT
+        NOT VALID,
+    FOREIGN KEY (project_employee_role_id)
+        REFERENCES project_employee_role (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE SET DEFAULT
+        NOT VALID
+);
+
+CREATE TABLE employee_personal_data
+(
+    id                      bigserial NOT NULL,
+    common_personal_data_id bigint    NOT NULL,
+    weighting_factor_id     bigint    NOT NULL,
+    employee_id             bigint    NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (employee_id)
+        REFERENCES employee (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE CASCADE,
+    FOREIGN KEY (common_personal_data_id)
+        REFERENCES common_personal_data (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE CASCADE,
+    FOREIGN KEY (weighting_factor_id)
+        REFERENCES weighting_factor (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE CASCADE
+);
+
+CREATE TABLE employee_professional_data
+(
+    id                          bigserial NOT NULL,
+    common_professional_data_id bigint    NOT NULL,
+    weighting_factor_id         bigint    NOT NULL,
+    employee_id                 bigint    NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (employee_id)
+        REFERENCES employee (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE CASCADE,
+    FOREIGN KEY (common_professional_data_id)
+        REFERENCES common_professional_data (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE CASCADE,
+    FOREIGN KEY (weighting_factor_id)
+        REFERENCES weighting_factor (id) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE CASCADE
 );
